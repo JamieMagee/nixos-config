c955dcdaaf1a17abbd94d76b934b73b8cc41ce0f
Jamie Magee <jamie.magee@gmail.com>
2020-02-25 16:58:08 +0100

    More complete bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,15 +20,6 @@ done
 # script values
 MOUNTOPTS=compression=lz4,xattr=sa,acltype=posixacl
 
-read -p "> Do you want to securely wipe $ENTRY ?" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sgdisk --zap-all /dev/device
-  cryptsetup open --type plain /dev/device container --key-file /dev/random
-  dd if=/dev/zero of=/dev/mapper/container status=progress
-  cryptsetup close container
-fi
-
 #----------------------------------------------------------------------
 # Create Partitions
 #----------------------------------------------------------------------
@@ -46,20 +37,45 @@ sleep 1 # udev race condition
 # Format & Mount Partitions
 #----------------------------------------------------------------------
 
-# EFI
-mkfs.fat -F32 -n EFI "$DISK-part1"
-
 # Create zpool
 zpool create -O mountpoint=none -R /mnt zpool "$DISK-part2"
 
-zfs create -o mountpoint=legacy zpool/root
-zfs create -o mountpoint=legacy zpool/home
+echo "Creating and mounting datasets in /mnt..."
 
-mount -t zfs zpool/root /mnt
+# / (root) datasets
+zfs create -o mountpoint=none -o canmount=off zpool/ROOT
+zfs create -o mountpoint=legacy -o canmount=on zpool/ROOT/nixos
+mount -t zfs zpool/ROOT/nixos /mnt
+zpool set bootfs=zpool/ROOT/nixos zpool
 
+# EFI
+mkfs.fat -F32 -n EFI "$DISK-part1"
 mkdir /mnt/boot
 mount "$DISK-part1" /mnt/boot
+
+# mount /nix outside of the root dataset
+zfs create -o mountpoint=none -o canmount=off zpool/NIX
+zfs create -o mountpoint=legacy -o canmount=on zpool/NIX/nix
+mkdir /mnt/nix
+mount -t zfs zpool/NIX/nix /mnt/nix
+
+# /home dataset
+zfs create -o mountpoint=legacy -o canmount=on zroot/HOME/home
 mkdir /mnt/home
-mount -t zfs zpool/home /mnt/home
+mount -t zfs zroot/HOME/home /mnt/home
 
 nixos-generate-config --root /mnt
+
+# Clone nixos-configuration repo
+mkdir -p /mnt/home/jamie/code
+echo "import /home/jamie/code/nixos-configuration \"$(hostname)\"" >/mnt/etc/nixos/configuration.nix
+cd /mnt/home/jamie/code
+git clone git://github.com/JamieMagee/nixos-configuration
+cd nixos-configuration
+
+mv /mnt/etc/nixos/hardware-configuration.nix "hardware-configuration/$(hostname).nix"
+
+./build
+nixos-install --system ./result
+cd /mnt/home/jamie
+chown -R 1000:1000 .
