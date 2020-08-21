e815bab6025abe0148843272984b299acbc2b509
Jamie Magee <jamie.magee@gmail.com>
2020-08-21 22:51:42 +0200

    create my user

---
+++ users/jamie/default.nix
@@ -0,0 +1,12 @@
+{
+  users.users.jamie = {
+    uid = 1000;
+    password = "nixos";
+    description = "default";
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIr1ZYIVMaWmhOVW3rqJS24Ffkin4XdxfMXFR5E4mt3W"
+    ];
+  };
+}
