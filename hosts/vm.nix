19665a0b1660df3d0512fc0c3549cf49ababd3a8
Jamie Magee <jamie.magee@gmail.com>
2020-08-21 22:52:08 +0200

    vm host

---
+++ hosts/vm.nix
@@ -0,0 +1,25 @@
+{
+  ### root password is empty by default ###
+  imports = [ 
+    ../profiles/ssh
+    ../users/jamie
+    ../users/root
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+
+  fileSystems."/" = { 
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = { 
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+  };
+
+  virtualisation.virtualbox.guest.enable = true;
+}
