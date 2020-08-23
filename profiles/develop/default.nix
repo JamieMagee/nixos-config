304cad8fbaea438496b16cfa31ee5216ac21ac80
Jamie Magee <jamie.magee@gmail.com>
2020-08-23 17:01:18 +0200

    dotnet sdk

--- profiles/develop/default.nix
+++ profiles/develop/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }: {
 
-  environment.systemPackages = with pkgs; [ nodejs-14_x ];
+  environment.systemPackages = with pkgs; [
+    nodejs-14_x
+    (with dotnetCorePackages; combinePackages [ sdk_3_1 sdk_3_0 sdk_2_1 ])
+  ];
 
 }
