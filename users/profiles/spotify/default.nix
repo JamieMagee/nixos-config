9a7b0db183c8628a7d0f12b42d0546d9afd01984
Jamie Magee <jamie.magee@gmail.com>
2020-09-03 13:25:59 +0200

    Add spotify

---
+++ users/profiles/spotify/default.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}:
+let
+  inherit (builtins) readFile;
+in {
+  services.spotifyd = {
+    enable = true;
+    settings = {
+      global = {
+        username = "Jammie001";
+        password = "${readFile ../../../secrets/spotifyd}";
+      };
+    };
+  };
+  home.packages = with pkgs; [
+    spotify-tui
+  ];
+  xdg.configFile."spotify-tui/client.yml".source = ../../../secrets/spotify-tui;
+}
