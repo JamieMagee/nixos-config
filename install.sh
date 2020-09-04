b269f80527e21ca7730043def4ac2ca7e78d3c08
Jamie Magee <jamie.magee@gmail.com>
2020-09-04 11:04:11 +0200

    workaround for udev race condition

--- install.sh
+++ install.sh
@@ -26,6 +26,9 @@ create_partition() {
     --new=1:0:+512MiB --typecode=1:ef00 --change-name=1:EFI \
     --new=2:0:0 --typecode=2:bf00 --change-name=2:zroot \
     "$DISK"
+
+    # udev race condition
+    sleep 1
 }
 
 format_partition() {
