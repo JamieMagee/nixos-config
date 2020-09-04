75e8c186d055838bc684a0a996a2b8d8f0794cc3
Jamie Magee <jamie.magee@gmail.com>
2020-09-04 14:42:02 +0200

    Set console.keyMap

--- local/locale.nix
+++ local/locale.nix
@@ -1,4 +1,10 @@
-{ ... }: {
+{ lib, ... }: {
+  console.keyMap = "uk";
+
+  # environment.sessionVariables = {
+  #   XKB_DEFAULT_LAYOUT = "uk,us";
+  # };
+
   i18n.defaultLocale = "en_GB.UTF-8";
   time.timeZone = "Europe/Copenhagen";
 }
