82efb66a58984040244b57e2fa9ad89fb92ca57d
Jamie Magee <jamie.magee@gmail.com>
2020-09-10 22:48:18 +0200

    set default keyboard layout

--- local/locale.nix
+++ local/locale.nix
@@ -1,9 +1,10 @@
 { lib, ... }: {
   console.keyMap = "uk";
 
-  # environment.sessionVariables = {
-  #   XKB_DEFAULT_LAYOUT = "uk,us";
-  # };
+  environment.sessionVariables = {
+    XKB_DEFAULT_LAYOUT = "gb,us";
+    XKB_DEFAULT_OPTIONS = "grp:alt_shift_toggle";
+  };
 
   i18n.defaultLocale = "en_GB.UTF-8";
   time.timeZone = "Europe/Copenhagen";
