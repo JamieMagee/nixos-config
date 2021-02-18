a807edba70772952cfcb63f9d9a86be73576f097
Jamie Magee <jamie.magee@gmail.com>
2021-02-17 20:35:44 -0800

    deafult shell

--- profiles/develop/fish/default.nix
+++ profiles/develop/fish/default.nix
@@ -1,3 +1,31 @@
+{ pkgs, ... }:
 {
-  
+  users.defaultUserShell = pkgs.fish;
+
+  environment = {
+    shellAliases = {
+      cat = "${pkgs.bat}/bin/bat";
+
+      df = "df -h";
+      du = "du -h";
+
+      ls = "${pkgs.exa}/bin/exa";
+      l = "ls -lhg --git";
+      la = "l -a";
+      t = "l -T";
+      ta = "la -T";
+
+      ps = "${pkgs.procs}/bin/procs";
+    };
+    systemPackages = with pkgs; [
+      bat
+      exa
+    ];
+  };
+
+  programs.fish = {
+    enable = true;
+
+    
+  };
 }
--- users/profiles/fish/default.nix
+++ users/profiles/fish/default.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 {
-  users.defaultUserShell = pkgs.fish;
-
   programs.fish = {
     enable = true;
     functions = {
